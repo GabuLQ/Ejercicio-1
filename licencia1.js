09c27ceb65a424ddc1fb3418e6c5fcc03ef8cd7a
GabuLQ <lopez.gabriela.ing.gti@gmail.com>
2024-06-19 22:33:14 -0700

    Add files via upload

---
+++ licencia1.js
@@ -0,0 +1,19 @@
+alert = ('Bienvenidos a nuestro sitio web!');
+
+var nombre = Luna;
+var edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert = ('Error! Completa todos los campos');
+
+let mensajeDeError = 'Error! Completa todos los campos';
+alert(mensajeDeError);
+
+let nombre = prompt('Cuál es su nombre?');
+let edad = prompt('Cuál es su edad?');
+
+let edad = prompt('Cuál es su edad?');
+if (edad >= 18) {
+    alert('Puede obtener su licencia de conducir!')
+}
